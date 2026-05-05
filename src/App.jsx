3d430f8955d415bfc534de9955373830fc3dddf5
“Nik <nikolaspreyes@gmail.com>
2026-05-05 12:44:12 -0700

    refactor: implement click to rotate and import all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import "./App.css";
 import CardFace from "./components/CardFace/CardFace.jsx";
+import cardback from "./images/cardback.png";
+import { useState } from "react";
+import "./App.css";
 
-function Card() {
+const images = import.meta.glob("./images/*.{png,jpg,jpeg,svg}", {
+  eager: true,
+});
+const imageArray = Object.values(images)
+  .map((module) => module.default)
+  .filter((path) => path.includes("cardfront"))
+  .map((path, i) => ({ url: path, id: i }));
+
+function Card({ url }) {
+  const [flipped, setFlipped] = useState(false);
   return (
-    <div className="relative w-full aspect-[137/274] [perspective:1000px] group">
+    <div
+      onClick={() => {
+        setFlipped((prev) => !prev);
+      }}
+      className="relative w-full aspect-[137/274] [perspective:1000px]"
+    >
       <div
-        className="relative w-full h-full transition-transform duration-500
+        className={`relative w-full h-full transition-transform duration-500
         [transform-style:preserve-3d]
-        group-hover:[transform:rotateY(180deg)] [will-change:transform]"
+        ${flipped ? "[transform:rotateY(180deg)]" : ""} [will-change:transform]`}
       >
-        <CardFace image="./images/cardfront-one.png" />
-        <CardFace image="./images/cardback.png" flipped />
+        <CardFace image={url} flipped={flipped} />
+        <CardFace image={cardback} isFront flipped={flipped} />
       </div>
     </div>
   );
@@ -19,14 +35,9 @@ function Card() {
 function Home({}) {
   return (
     <div className="p-[20px] grid grid-cols-[repeat(auto-fit,minmax(180px,1fr))] min-h-screen gap-x-[30px] gap-y-[20px] overflow-y-auto bg-gray-800 bg-gradient-to-b from-[#2b0e10] to-[#07080d]">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {imageArray.map((image) => (
+        <Card url={image.url} key={image.id} />
+      ))}
     </div>
   );
 }
--- src/components/CardFace/CardFace.jsx
+++ src/components/CardFace/CardFace.jsx
@@ -1,13 +1,13 @@
 const clip =
   "[clip-path:polygon(0%_0%,100%_0%,100%_calc(100%_-_11px),calc(100%_-_9px)_100%,0%_100%)]";
 
-function CardFace({ image, flipped }) {
+function CardFace({ image, isFront, flipped }) {
   return (
     <div
-      className={`absolute inset-0 [backface-visibility:hidden] transition-opacity duration-500 $ ${
-        flipped
-          ? "[transform:rotateY(180deg)] opacity-0 group-hover:opacity-100"
-          : "opacity-100 group-hover:opacity-0"
+      className={`absolute inset-0 [backface-visibility:hidden] transition-opacity ${
+        isFront
+          ? `[transform:rotateY(180deg)] ${flipped ? "opacity-100 duration-300" : "opacity-0 duration-300"}`
+          : `${flipped ? "opacity-0 duration-300" : "opacity-100 duration-600"}`
       }`}
     >
       <div
@@ -15,9 +15,9 @@ function CardFace({ image, flipped }) {
             [clip-path:polygon(0%_0%,100%_0%,100%_calc(100%_-_11px),calc(100%_-_9px)_100%,0%_100%)]"
       >
         <div
-          className={`absolute inset-[1px]
-              bg-[url('${image}')] bg-cover bg-center bg-no-repeat
+          className={`absolute inset-[1px] bg-cover bg-center bg-no-repeat
               [clip-path:polygon(0%_0%,100%_0%,100%_calc(100%_-_11px),calc(100%_-_9px)_100%,0%_100%)]`}
+          style={{ backgroundImage: `url('${image}')` }}
         ></div>
       </div>
     </div>
